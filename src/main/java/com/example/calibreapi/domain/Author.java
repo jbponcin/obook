d7f6efa154a8a4a6d4472f5b56838f00cd726df2
Jean-Baptiste Poncin <jbponcin@gmail.com>
2025-12-26 18:48:43 +0100

    Add Getter and Setters for all entities

--- src/main/java/com/example/calibreapi/domain/Author.java
+++ src/main/java/com/example/calibreapi/domain/Author.java
@@ -21,4 +21,36 @@ public class Author {
     private String link;
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
--- src/main/java/com/example/calibreapi/domain/Publisher.java
+++ src/main/java/com/example/calibreapi/domain/Publisher.java
@@ -18,4 +18,28 @@ public class Publisher {
     private String sort;
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
 }
