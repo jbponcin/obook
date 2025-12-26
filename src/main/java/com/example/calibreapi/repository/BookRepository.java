795ff90eeb7c7aee1bc3cb441e3bbe408b4d33fe
Jean-Baptiste Poncin <jbponcin@gmail.com>
2025-12-26 18:58:20 +0100

    Add the search of books by Ids

--- src/main/java/com/example/calibreapi/repository/BookRepository.java
+++ src/main/java/com/example/calibreapi/repository/BookRepository.java
@@ -3,7 +3,15 @@ package com.example.calibreapi.repository;
 import com.example.calibreapi.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Collection;
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @RestResource(path = "byIds")
+    List<Book> findByIdIn(@Param("ids") Collection<Long> ids);
 }
